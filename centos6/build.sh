d822b87220ba0fcdedb6ba9b4772912c53fb44a0
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2021-08-26 15:55:56 -0400

    install python 3 in centos 6

--- centos6/build.sh
+++ centos6/build.sh
@@ -8,7 +8,5 @@ cd ..
 cp *.py centos6
 cp -r helpers centos6/
 cd centos6
-/Users/tim/Library/Python/3.8/bin/3to2 -w -n __init__.py
-/Users/tim/Library/Python/3.8/bin/3to2 -w -n helpers/__init__.py
-docker build --build-arg requirements="$(cat ../requirements.txt | tr "\n" " ")" -t terf/centos6-pyinstaller .
+docker build --progress=plain --build-arg requirements="$(cat ../requirements.txt | tr "\n" " ")" -t terf/centos6-pyinstaller .
 docker run -it --rm -v $PWD:/opt/build -w /opt/build -e PYTHONPATH=/opt/build terf/centos6-pyinstaller --onefile --noconfirm __init__.py
